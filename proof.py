ebeb886bc1a867472abd7d803238f1e7134e3262
Gaston Fenske <63388116+GastonFenske@users.noreply.github.com>
2022-11-20 06:55:11 -0300

    Update proof.py

--- proof.py
+++ proof.py
@@ -3,7 +3,7 @@ import logging
 import time
 #logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(message)s')
 print("Accediendo...")
-Iq=IQ_Option("s.fenske@alumno.um.edu.ar","perrito2")
+Iq=IQ_Option("s.fenske@alumno.um.edu.ar","")
 Iq.connect()#connect to iqoption
 goal="AUDCAD-OTC"
 size="all"#size=[1,5,10,15,30,60,120,300,600,900,1800,3600,7200,14400,28800,43200,86400,604800,2592000,"all"]
