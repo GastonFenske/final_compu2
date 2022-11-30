d3e3375f8fc4f3c59c56481df01870a0d03d2a2b
GastonFenske <thesacha2000f@gmail.com>
2022-11-30 10:36:10 -0300

    push for douglas v2

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from constants import *
 from server import Server
 from server.router import route, get_fun_by_route, get_routes
 
-# connector = Connector(EMAIL, PASSWORD)
+# connector = Connector(EMAIL, PASSWORD
 # if connector.get_connect():
 #     print('Connected')
 # else:
@@ -15,6 +15,7 @@ from server.router import route, get_fun_by_route, get_routes
 # # get all open
 # open_markets = OpenMarkets(connector)
 # print(open_markets.get_open_markets())
+
 
 # print(get_routes)
 
